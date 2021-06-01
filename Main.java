889560d9358ec22003ea039191023f1d8490b21d
Enoc-Mena <77991403+Enoc-Mena@users.noreply.github.com>
2021-06-01 12:56:42 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+package com.simplemath;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String firstNum, secondNum;
+
+        //Gets num 1
+        Scanner numOne = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        firstNum = numOne.nextLine();
+        int parseNumOne = Integer.parseInt(firstNum);
+
+        //Gets num 2
+        Scanner numTwo = new Scanner(System.in);
+        System.out.print("Enter second number: ");
+        secondNum = numTwo.nextLine();
+        int parseNumTwo = Integer.parseInt(secondNum);
+
+        //Math output
+        System.out.print(parseNumOne + " + " + parseNumTwo + " = " + (parseNumOne+parseNumTwo));
+        System.out.println("");
+        System.out.print(parseNumOne + " - " + parseNumTwo + " = " + (parseNumOne-parseNumTwo));
+        System.out.println("");
+        System.out.print(parseNumOne + " * " + parseNumTwo + " = " + (parseNumOne*parseNumTwo));
+        System.out.println("");
+        System.out.print(parseNumOne + " / " + parseNumTwo + " = " + (parseNumOne/parseNumTwo));
+
+    }
+}
